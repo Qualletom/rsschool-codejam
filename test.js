d7d0b85be56b19c32d20dab81518bf5567c79f01
Aliaksey <leshich2011@gmail.com>
2018-10-28 19:19:53 +0300

    feat: add tests for make function

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const sumOfOther = require('./src/sumOfOther');
+const make = require('./src/make');
 
 describe('sumOfOther', () => {
   it('1', () => {
@@ -15,5 +16,31 @@ describe('sumOfOther', () => {
   it('3', () => {
     const result = sumOfOther([-1, -10, -20, -34, -120, -341, -52, -41]);
     assert.deepEqual(result, [-618, -609, -599, -585, -499, -278, -567, -578]);
+  });
+});
+
+function sum(a, b) {
+  return a + b;
+}
+
+describe('make', () => {
+  it('1', () => {
+    const result = make(15)(34, 21, 666)(41)(sum);
+    assert.equal(result, 777);
+  });
+
+  it('2', () => {
+    const result = make(15)(2)(sum);
+    assert.equal(result, 17);
+  });
+
+  it('3', () => {
+    const result = make(15)(2, -10)(100)(65, 98, 57)(21, 65)(sum);
+    assert.equal(result, 413);
+  });
+
+  it('4', () => {
+    const result = make(15, 10, 258, 74, 823, 876)(-20)(sum);
+    assert.equal(result, 2036);
   });
 });
